415f93d8460157335a6d9edc304a5b1f32809a25
Daniel Newton <djpnewton@gmail.com>
2017-09-19 18:02:39 +1200

    remove -1 from max int (2^bits) in add/subtract methods; add print out of keys for storage

--- k2o3.py
+++ k2o3.py
@@ -5,7 +5,7 @@ import binascii
 from mnemonic import Mnemonic
 from Levenshtein import distance
 
-VERSION=2
+VERSION=3
 
 def random_seed(bits):
     return os.urandom(bits/8)
@@ -58,17 +58,29 @@ def sub(a, b, bits):
     # then roll over backward from the maximum int given the bit size
     result = a - b
     if result < 0:
-        result = (2**bits - 1) + result
+        result = (2**bits) + result
     return result
 
 def add(a, b, bits):
     # add a to b and if the result is greater then the maximum int
     # given the bit size then take the result modulo of the max int
     result = a + b
-    max = 2**bits - 1
+    max = 2**bits
     if result > max:
         result = result % max
     return result
+
+def print_words(words, max_per_line=12, offset1=0, offset2=7):
+    words = words.split(" ")
+    c = 0
+    line = " " * offset1
+    for i in range(len(words)):
+        line += words[i] + " "
+        c += 1
+        if c % max_per_line == 0:
+            print line,
+            line = "\n" + " " * offset2
+    print
 
 def test_key_splitting(original_key, bits):
     #
@@ -86,16 +98,23 @@ def test_key_splitting(original_key, bits):
     key_c = sub(key_a, key_b, bits)
 
     # test
-    print "original key   :", int_to_mnemonic(original_key, bits)
-    print "key_a          :", int_to_mnemonic(key_a, bits)
-    print "key_b          :", int_to_mnemonic(key_b, bits)
-    print "key_c          :", int_to_mnemonic(key_c, bits)
+    print "original key   :",
+    print_words(int_to_mnemonic(original_key, bits), offset2=17)
+    print "key_a          :",
+    print_words(int_to_mnemonic(key_a, bits), offset2=17)
+    print "key_b          :",
+    print_words(int_to_mnemonic(key_b, bits), offset2=17)
+    print "key_c          :",
+    print_words(int_to_mnemonic(key_c, bits), offset2=17)
     r1 = add(key_a, key_b, bits)
-    print "key_a + key_b  :", int_to_mnemonic(r1, bits)
+    print "key_a + key_b  :",
+    print_words(int_to_mnemonic(r1, bits), offset2=17)
     r2 = add(add(key_b, key_b, bits), key_c, bits)
-    print "2*key_b + key_c:", int_to_mnemonic(r2, bits)
+    print "2*key_b + key_c:",
+    print_words(int_to_mnemonic(r2, bits), offset2=17)
     r3 = sub(add(key_a, key_a, bits), key_c, bits)
-    print "2*key_a - key_c:", int_to_mnemonic(r3, bits)
+    print "2*key_a - key_c:",
+    print_words(int_to_mnemonic(r3, bits), offset2=17)
 
     assert(r1 == original_key)
     assert(r2 == original_key)
@@ -105,6 +124,55 @@ def test_key_splitting(original_key, bits):
     assert(key_a == seed_to_int(mnemonic_to_seed(int_to_mnemonic(key_a, bits))))
     assert(key_b == seed_to_int(mnemonic_to_seed(int_to_mnemonic(key_b, bits))))
     assert(key_c == seed_to_int(mnemonic_to_seed(int_to_mnemonic(key_c, bits))))
+
+    return key_a, key_b, key_c
+
+def test_key_print(key_a, key_b, key_c, bits):
+    apb  = "(key_a + key_b) %% 2^%d" % bits
+    _2bpc = "((2*key_b %% 2^%d) + key_c) %% 2^%d" % (bits, bits)
+    ##TODO: need symbol for underflow from below 0 to 2^bits!
+    _2amc = "((2*key_a %% 2^%d) - key_c) ?%%? 0" % (bits,)
+    print
+    print "key_a:"
+    print "====="
+    print "bip39:",
+    print_words(int_to_mnemonic(key_a, bits))
+    print "hex  :", "{0:#0{1}x}".format(key_a, 2 + bits/8)
+    print
+    print "recover via:"
+    print "============"
+    print apb
+    print " or"
+    print _2amc
+    print
+    print "---8<-------------------------------------------------------"
+    print
+    print "key_b"
+    print "====="
+    print "bip39:",
+    print_words(int_to_mnemonic(key_b, bits))
+    print "hex  :", "{0:#0{1}x}".format(key_b, 2 + bits/4)
+    print
+    print "recover via:"
+    print "============"
+    print apb
+    print " or"
+    print _2bpc
+    print
+    print "---8<-------------------------------------------------------"
+    print
+    print "key_c:"
+    print "====="
+    print "bip39:",
+    print_words(int_to_mnemonic(key_a, bits))
+    print "hex  :", "{0:#0{1}x}".format(key_c, 2 + bits/8)
+    print
+    print "recover via:"
+    print "============"
+    print _2bpc
+    print " or"
+    print _2amc
+    print
 
 def test_mnemonic_words():
     # test mnemonic word guessing
@@ -117,8 +185,12 @@ if __name__ == "__main__":
     print
     print ":::"
     print
-    bits = 128
-    test_key_splitting(seed_to_int(random_seed(bits)), bits)
+    bits = 256
+    key_a, key_b, key_c = test_key_splitting(seed_to_int(random_seed(bits)), bits)
+    print
+    print ":::"
+    print
+    test_key_print(key_a, key_b, key_c, bits)
     print
     print ":::"
     print
